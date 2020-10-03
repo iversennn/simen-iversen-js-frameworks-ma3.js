70726bd70e1f47cce681e7ec777dd4730c54f634
Simen Iversen <simen.m.iversen@gmail.com>
2020-10-03 16:24:30 +0200

    Second commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "../layout/Heading";
+import HomeContent from "../home/HomeContent";
+
+export function Home(){
+    return(
+        <>
+            <Heading title="Home"/>
+            <HomeContent />
+        </>
+    )
+}
+
+export default Home;
---
+++ src/components/home/HomeContent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HomeContent() {
+	return (
+		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, reiciendis. Cumque temporibus obcaecati rem tempore architecto corporis! Totam, qui. Culpa sint iure voluptas illum deserunt rem, natus officiis earum magnam?</p>
+	);
+}
+
+export default HomeContent;
